45b2a0daca703633b755dc3e907e93ed57c6c61f
Bruno Tavares <tavaresbm@gmail.com>
2015-07-22 10:19:23 +0100

    Updated dev config which had experimental configs

--- config/dev.exs
+++ config/dev.exs
@@ -4,42 +4,26 @@ config :logger,
   utc_log: true,
   truncate: 8192,
   sync_threshold: 40,
-  # handle_otp_reports: true,
-  # handle_sasl_reports: true,
   discard_threshold_for_error_logger: 500,
   compile_time_purge_level: :debug
 
 config :logger, :console,
   level: :error,
   format: "$date $time [$level] $levelpad$node $metadata $message\n",
-  # utc_log: true,
-  # truncate: 8192,
-  # sync_threshold: 40,
   metadata: [:module, :line, :function]
-  # handle_otp_reports: true,
-  # handle_sasl_reports: true,
-  # discard_threshold_for_error_logger: 500,
-  # compile_time_purge_level: :debug
 
-# config :sasl,
-#   errlog_type: :error,
-#   utc_log: true
-
-config :logger, :TestSyslogError,
+config :logger, :exsyslog_error,
   level: :error,
   format: "\n$date $time [$level] $levelpad$node $metadata $message\n",
   metadata: [:module, :line, :function],
-  ident: "TestSyslogError",
+  ident: "SyslogError",
   facility: :local0,
-  option: [:pid, :cons],
-  wrap_to_json: true
+  option: [:pid, :cons]
 
-config :logger, :TestSyslogInfo,
+config :logger, :exsyslog_error,
   level: :info,
   format: "\n$date $time [$level] $levelpad$node $metadata $message\n",
   metadata: [:module, :line, :function],
-  ident: "TestSyslogInfo",
+  ident: "SyslogInfo",
   facility: :local0,
-  option: [:pid, :cons],
-  wrap_to_json: true
-
+  option: [:pid, :cons]
