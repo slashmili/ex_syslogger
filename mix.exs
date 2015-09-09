fa420411dce4aaf2f97da4666da7986b31f3e7a3
Bruno Matos Tavares <bruno.matos.tavares@gmail.com>
2015-09-09 10:20:03 +0100

    0.1.0 => 1.0.0 No bugs reported

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exsyslog.Mixfile do
 
   def project do
     [app: :exsyslog,
-     version: "0.1.0",
+     version: "1.0.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
